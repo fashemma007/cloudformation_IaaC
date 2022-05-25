e528fb262ac6d39ec60331b38c139b7376e37af1
fashemma007 <fasogbaemmanuel@gmail.com>
2022-05-25 06:44:24 +0100

    provisioning VPC and IGW

--- supporting_material/create.sh
+++ supporting_material/create.sh
@@ -1,1 +1,5 @@
-aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation create-stack --stack-name $1 --template-body file://$2 --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+
+
+
+aws cloudformation create-stack --stack-name step_by_step --template-body file://step_by_step.yml --parameters file://s_b_s_parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
